1d70f5c5caf0ab07548b609a9b6f5829965cbe7e
Willian Vargas <willianvargas1995@hotmail.com>
2024-11-11 20:57:38 -0500

    change code

--- app.R
+++ app.R
@@ -1,12 +1,11 @@
-# app.R
+
 library(plumber)
 
-# Esta ruta devolverá un mensaje en HTML
-#* @get /hello
+
 function() {
   "<h1>Hello from R with Plumber!</h1>"
 }
 
-# Inicia el servidor en el puerto 8787
-pr <- plumb("app.R")  # Carga el archivo
+
+pr <- plumb("app.R") 
 pr$run(host = "0.0.0.0", port = 8787)
